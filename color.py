46ba53aca934f8f0c2fcf9bb792fabb5ddcb1955
Daniel Fletcher <90067982+DanielTheFletch@users.noreply.github.com>
2023-12-21 17:46:22 -0500

    Color class skeleton, rgb function

--- color.py
+++ color.py
@@ -6,8 +6,41 @@
 # Color class and related functionality
 
 class Color:
-    ...
+    """Color class"""
+
+    def __init__(self):
+        self.red = 0
+        self.green = 0
+        self.blue = 0
+
+    @property
+    def red(self) -> int:
+        return self._red
+    
+    @red.setter
+    def red(self, red: int):
+        self._red = red
+
+    @property
+    def green(self) -> int:
+        return self._green
+    
+    @green.setter
+    def green(self, green: int):
+        self._green = green
+
+    @property
+    def blue(self) -> int:
+        return self._blue
+    
+    @blue.setter
+    def blue(self, blue: int):
+        self._blue = blue
 
 
-def rgb(r: int, g: int, b: int) -> Color:
-    ...
+def rgb(red: int, green: int, blue: int) -> Color:
+    color = Color()
+    color.red = red
+    color.green = green
+    color.blue = blue
+    return color
