556a821588c83d515f89c7bf70a8e421169534a1
Daniel Fletcher <90067982+DanielTheFletch@users.noreply.github.com>
2023-12-29 02:47:57 -0500

    Add test file for use with pytest

---
+++ test_project.py
@@ -0,0 +1,23 @@
+# Daniel Fletcher
+# Harvard CS50P 2023
+# Final Project
+
+# test_project.py
+# Contains test cases for rgb, hexcode, and css functions
+
+
+from project import rgb, hexcode, css
+from color import Color
+from colorstr import ColorStr
+
+
+def test_rgb():
+    ...
+
+
+def test_hexcode():
+    ...
+
+
+def test_css():
+    ...
