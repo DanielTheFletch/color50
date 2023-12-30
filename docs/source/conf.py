2f45585fdff8391015383c29eaac1152d7f53bc8
Daniel Fletcher <90067982+DanielTheFletch@users.noreply.github.com>
2023-12-30 13:02:38 -0500

    Add more information to conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,8 +14,8 @@ project = 'color50'
 copyright = '2023, Daniel Fletcher'
 author = 'Daniel Fletcher'
 
-version = 1.0
-release = 1.0
+version = '1.0'
+release = '1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -23,9 +23,16 @@ release = 1.0
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode'
 ]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
@@ -34,3 +41,5 @@ templates_path = ['_templates']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
+
+epub_show_urls = 'footnote'
