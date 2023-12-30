41f6a898195f2cf5a5d0e646bed7732d736e4d5f
Daniel Fletcher <90067982+DanielTheFletch@users.noreply.github.com>
2023-12-30 02:58:39 -0500

    __init__.py: docstrings first pass

--- color50/__init__.py
+++ color50/__init__.py
@@ -1,3 +1,20 @@
+"""A Python package for more colorful printing at the terminal.
+
+Created as a final project submission for Harvard University's CS50P course,
+``color50`` is a package designed to add a little extra flavor to standard
+output print statements. Who knows, maybe future CS50 students may find this
+package useful (or fun)!
+
+Modules included:
+    - color
+    - colorstr
+    - constants
+    - core_functions
+
+View the :doc:`full package documentation </color50>` for more details.
+
+"""
+
 # Daniel Fletcher
 # Harvard CS50P 23023
 # Final Project
