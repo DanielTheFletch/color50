61cb54896d4cbdb3293c5f89ce5e81b68c5d7214
Daniel Fletcher <90067982+DanielTheFletch@users.noreply.github.com>
2023-12-29 23:58:44 -0500

    Reorganize documentation structure

---
+++ docs/source/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../color50/'))
+
+project = 'color50'
+copyright = '2023, Daniel Fletcher'
+author = 'Daniel Fletcher'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode'
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,19 @@
+.. color50 documentation master file, created by
+   sphinx-quickstart on Fri Dec 29 23:41:05 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to color50's documentation!
+===================================
+
+Some text.
+
+
+Contents
+--------
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   Home <self>
