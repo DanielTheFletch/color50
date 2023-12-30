4427a4cb57223a4b3ed04581c87f470083686e39
Daniel Fletcher <90067982+DanielTheFletch@users.noreply.github.com>
2023-12-30 18:07:19 -0500

    Tidy up conf.py configuration file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,25 +1,22 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+# Daniel Fletcher
+# Harvard CS50P 2023
+# Final Project
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+# conf.py
+# Configuration file for Sphinx documentation builder
 
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../color50/'))
 
+# General configuration
 project = 'color50'
 copyright = '2023, Daniel Fletcher'
 author = 'Daniel Fletcher'
-
 version = '1.0'
 release = '1.0'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
+# Extension settings
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
@@ -27,19 +24,15 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode'
 ]
-
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
 }
 intersphinx_disabled_domains = ['std']
 
+# Path settings
 templates_path = ['_templates']
 
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
+# Output settings
 html_theme = 'sphinx_rtd_theme'
-
 epub_show_urls = 'footnote'
